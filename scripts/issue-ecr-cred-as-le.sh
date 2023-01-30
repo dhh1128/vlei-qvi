110a8388817e6c307398b8bd345bb9a66ccd28df
Daniel Hardman <daniel.hardman@gmail.com>
2023-01-30 17:02:11 +0100

    share cred files via s3
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- scripts/issue-ecr-cred-as-le.sh
+++ scripts/issue-ecr-cred-as-le.sh
@@ -29,4 +29,17 @@ kli saidify --file data/legal-entity-ecr-edge-data.json
 # Prepare the RULES section
 cp "${QAR_SCRIPT_DIR}/ecr-rules.json" "${QAR_DATA_DIR}/rules.json"
 
+# Make sure old credential isn't left laying around by mistake to confuse us.
+rm -f data/ecr.json
+
+# Automatically upload the newly generated ACDC to S3 so others can download it
+# without this user having to start a new ssh session and run scp. This share
+# script will start, move into the background to allow the kli vc issue command
+# to run, and lurk up to 30 secs for the ACDC to appear. Once it appears, the
+# script will automatically perform the upload and exit. The user will perceive
+# the upload to happen after issuance. If the script fails for some reason, you
+# can re-run: share-via-s3 upload --file data/ecr.json --now. This will cause it
+# to immediately upload data/ecr.json.
+share-via-s3 upload --file data/ecr.json &
+
 kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --private --registry-name "${QAR_REG_NAME}" --schema EEy9PkikFcANV1l7EHukCeXqrzT1hNZjGlUk7wuMO5jw --recipient "${recipient}" --data @"data/legal-entity-ecr-data.json" --edges @"data/legal-entity-ecr-edge-data.json" --rules @"data/rules.json" --out "data/ecr.json"
--- scripts/join-legal-entity-ecr-credential.sh
+++ scripts/join-legal-entity-ecr-credential.sh
@@ -15,7 +15,14 @@ passcode="$(get_passcode $1)"
 # Capture password
 passcode="$(get_passcode $1)"
 
-read -p "Enter the filename of the new credential: " -r filename
+# Make sure old credential isn't left laying around by mistake to confuse us.
+rm -f data/ecr.json
+
+read -p "Enter the name on S3 of the new credential: " -r obj
+
+# Download the newly generated ACDC from S3 so we can use it to
+# join, without having to start a new ssh session and run scp.
+share-via-s3 download --obj "$obj" --file data/ecr.json
 
 kli vc issue --name "${QAR_NAME}" --passcode "${passcode}" --alias "${QAR_AID_ALIAS}" --credential @"${filename}"
 
