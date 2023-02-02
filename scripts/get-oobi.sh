53aac699a0e426ed79bce6664ef1e8a1f9b8db89
Daniel Hardman <daniel.hardman@gmail.com>
2023-02-02 22:51:18 +0100

    add script tweaks from Ruth
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- scripts/get-oobi.sh
+++ scripts/get-oobi.sh
@@ -13,4 +13,7 @@ source $PWD/source.sh
 passcode=$(get_passcode $1)
 
 echo ""
-kli contacts list --name "${QAR_NAME}" --passcode "${passcode}" | jq '"Alias: "+.alias+"\n\rAID:   "+.id+"\n\rOOBI: "+.oobi+"\n\rAuthenticated: "+ if .challenges | length > 0 then "True" else "False" end +"\n\r"' --raw-output
+#kli contacts list --name "${QAR_NAME}" --passcode "${passcode}" | jq '"Alias: "+.alias+"\n\rAID:   "+.id+"\n\rOOBI: "+.oobi+"\n\rAuthenticated: "+ if .challenges | length > 0 then "True" else "False" end +"\n\r"' --raw-output
+
+OOBI=`kli contacts list --name "${QAR_NAME}" --passcode "${passcode}" | jq '.oobi' --raw-output`
+echo "$OOBI"
